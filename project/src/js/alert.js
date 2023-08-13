408482a41da4ee3a99cf0450349fc8db2ad6ee3f
Tomer Kedem <tompha1@hotmail.com>
2023-08-13 09:56:50 +0300

    Delete zip file

--- project/src/js/alert.js
+++
@@ -1,7 +0,0 @@
-document.querySelector(".title").textContent = "The text from alerto.js"
-
-const message = () =>{
-    console.log("The Javascript was transplied to ES5!");
-}
-
-message();
--- project/src/js/project.js
+++
@@ -1,3 +0,0 @@
-document.title = "Gulp example file with minification";
-
-document.querySelector(".title").textContent= "My awesome Gulpy file";
